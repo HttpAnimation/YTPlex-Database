8de60762a2122276f7a30f0cdc68c5747d2e292b
HtpAnimation <shawndouglasyt@gmail.com>
2024-04-27 20:03:19 -0700

    SML Movie： Shrek's Costume [PDvKy6v52Fs].webm

--- SML/Season-1.sh
+++ SML/Season-1.sh
@@ -5,7 +5,7 @@ yt-dlp https://youtu.be/Q7R9bQaconY
 mv *.webm "S01E02 Shrek's Big Crap.webm"
 
 yt-dlp https://www.youtube.com/watch?v=PDvKy6v52Fs
-mv *.webm "S01E04 Shrek's Costume.webm"
+mv "SML Movie： Shrek's Costume [PDvKy6v52Fs].webm" "S01E04 Shrek's Costume.webm"
 
 yt-dlp https://youtu.be/mbUl1ZpBI4M
 mv "SML Movie： How To Save Princess Peach, The Black Yoshi Way？ [mbUl1ZpBI4M].webm" "S01E03 SML Movie: How to Save Princess Peach, the Black Yoshi Way?.webm"
