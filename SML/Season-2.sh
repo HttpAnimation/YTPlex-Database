8aa337833a2a2db7924bc524345ba62dd8fb2c1e
HtpAnimation <shawndouglasyt@gmail.com>
2024-04-27 20:24:46 -0700

    removes all downloads

--- SML/Season-2.sh
+++ SML/Season-2.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 # Main Downloads | Download -> Rename
-yt-dlp https://youtu.be/Q7R9bQaconY --sponsorblock-remove all -o "S01E02 Shrek's Big Crap.webm"
-
-yt-dlp https://www.youtube.com/watch?v=PDvKy6v52Fs --sponsorblock-remove all -o "S01E04 Shrek's Costume.webm"
-
-yt-dlp https://youtu.be/mbUl1ZpBI4M --sponsorblock-remove all -o "S01E03 SML Movie: How to Save Princess Peach, the Black Yoshi Way?.webm"
-
-yt-dlp https://youtu.be/YI0WT0G3hQg --sponsorblock-remove all -o "S01E01 The Nintendo DSi.webm"
 
 # Final move
 if [ ! -d "season 2" ]; then
