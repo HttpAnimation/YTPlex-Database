af20d9d1576749fadccb1f87adcdb0e7551ea6f2
HtpAnimation <shawndouglasyt@gmail.com>
2024-04-27 19:57:14 -0700

    reformat

--- SML/Season-1.sh
+++ SML/Season-1.sh
@@ -1,16 +1,9 @@
 mkdir season\ 1
 
-# Main Downloads
-yt-dlp https://youtu.be/Q7R9bQaconY
-yt-dlp https://www.youtube.com/watch?v=PDvKy6v52Fs
-yt-dlp https://youtu.be/mbUl1ZpBI4M
-yt-dlp https://youtu.be/YI0WT0G3hQg
+# Main Downloads | Download -> Rename
+ 
 
-# Renames for Seasons and EP's
-mv *.webm "S01E04 Shrek's Costume.webm"
-mv *.webm "S01E02 Shrek's Big Crap.webm"
-mv *.webm "S01E03 SML Movie: How to Save Princess Peach, the Black Yoshi Way?.webm"
-mv *.webm "S01E02 The Nintendo DSi"
+
 
 # Final move
 mv *.webm season\ 1/
