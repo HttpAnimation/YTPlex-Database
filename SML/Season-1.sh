d2bd96329c60d921b53330aa68cbe7669d04d2e9
HtpAnimation <shawndouglasyt@gmail.com>
2024-04-27 19:57:30 -0700

    yt-dlp https://www.youtube.com/watch?v=PDvKy6v52Fs mv *.webm "S01E04 Shrek's Costume.webm"

--- SML/Season-1.sh
+++ SML/Season-1.sh
@@ -4,7 +4,8 @@ mkdir season\ 1
 yt-dlp https://youtu.be/Q7R9bQaconY
 mv *.webm "S01E02 Shrek's Big Crap.webm"
 
-
+yt-dlp https://www.youtube.com/watch?v=PDvKy6v52Fs
+mv *.webm "S01E04 Shrek's Costume.webm"
 
 # Final move
 mv *.webm season\ 1/
