8996cea417a68cba20627d7ebcaab8c3a561c0ac
HtpAnimation <shawndouglasyt@gmail.com>
2024-04-27 19:55:57 -0700

    comment

--- SML/Season-1.sh
+++ SML/Season-1.sh
@@ -1,6 +1,13 @@
 mkdir season\ 1
+
+# Main Downloads
 yt-dlp https://youtu.be/Q7R9bQaconY
 yt-dlp https://www.youtube.com/watch?v=PDvKy6v52Fs
 yt-dlp https://youtu.be/mbUl1ZpBI4M
 yt-dlp https://youtu.be/YI0WT0G3hQg
+
+# Renames for Seasons and EP's
+
+
+# Final move
 mv *.webm season\ 1/
