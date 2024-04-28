58f264c041a6b28cf55a2aa24a57063aae321e12
HtpAnimation <shawndouglasyt@gmail.com>
2024-04-27 19:56:37 -0700

    new renames

--- SML/Season-1.sh
+++ SML/Season-1.sh
@@ -7,7 +7,10 @@ yt-dlp https://youtu.be/mbUl1ZpBI4M
 yt-dlp https://youtu.be/YI0WT0G3hQg
 
 # Renames for Seasons and EP's
-
+mv *.webm "S01E04 Shrek's Costume.webm"
+mv *.webm "S01E02 Shrek's Big Crap.webm"
+mv *.webm "S01E03 SML Movie: How to Save Princess Peach, the Black Yoshi Way?.webm"
+mv *.webm "S01E02 The Nintendo DSi"
 
 # Final move
 mv *.webm season\ 1/
