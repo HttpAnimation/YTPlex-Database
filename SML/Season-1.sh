faaa91170bac67ad42a8e41a96f6fa5f488d728b
HtpAnimation <shawndouglasyt@gmail.com>
2024-04-27 19:47:58 -0700

    format

---
+++ SML/Season-1.sh
@@ -0,0 +1,9 @@
+mkdir season\ 1
+yt-dlp https://youtu.be/Q7R9bQaconY
+mv "Shrek's Big Crap" "/season 1/S01E02 Shrek's Big Crap"
+yt-dlp https://www.youtube.com/watch?v=PDvKy6v52Fs
+mv "Shrek's Costume" "/season 1/S01E04 Shrek's Costume"
+yt-dlp https://youtu.be/mbUl1ZpBI4M
+mv "SML Movie: How to Save Princess Peach, the Black Yoshi Way?" "/season 1/S01E03 SML Movie: How to Save Princess Peach, the Black Yoshi Way?"
+yt-dlp https://youtu.be/YI0WT0G3hQg
+mv "The Nintendo DSi" "/season 1/S01E02 The Nintendo DSi"
