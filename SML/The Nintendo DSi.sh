58c24d0691733658b7c1e5b8279b60cb60612611
HtpAnimation <shawndouglasyt@gmail.com>
2024-04-27 20:14:33 -0700

    02 -> 01

--- SML/The Nintendo DSi.sh
+++ SML/The Nintendo DSi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yt-dlp https://youtu.be/YI0WT0G3hQg --sponsorblock-remove all -o "S01E02 The Nintendo DSi.webm"
+yt-dlp https://youtu.be/YI0WT0G3hQg --sponsorblock-remove all -o "S01E01 The Nintendo DSi.webm"
 
 # Suicide
 rm The Nintendo DSi.sh
