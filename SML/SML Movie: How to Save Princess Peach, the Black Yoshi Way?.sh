b5ea7951b6d39d0b29f75ff1685d53601633a297
HtpAnimation <shawndouglasyt@gmail.com>
2024-04-27 20:12:29 -0700

    rm "SML Movie: How to Save Princess Peach, the Black Yoshi Way?.sh"

--- SML/SML Movie: How to Save Princess Peach, the Black Yoshi Way?.sh
+++ SML/SML Movie: How to Save Princess Peach, the Black Yoshi Way?.sh
@@ -3,4 +3,4 @@
 yt-dlp https://youtu.be/mbUl1ZpBI4M --sponsorblock-remove all -o "S01E03 SML Movie: How to Save Princess Peach, the Black Yoshi Way?.webm"
 
 # Suicide
-rm Season-1.sh
+rm "SML Movie: How to Save Princess Peach, the Black Yoshi Way?.sh"
